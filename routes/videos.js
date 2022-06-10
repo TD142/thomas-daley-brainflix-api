723326f53d7816e7b53edd29a56a0f42557e76bb
Thomas Daley <prayerproduction@gmail.com>
2022-06-10 08:44:52 +0100

    comment delete functionality added

--- routes/videos.js
+++ routes/videos.js
@@ -36,11 +36,13 @@ router.get("/videos", (req, res) => {
 });
 
 router.get("/videos/:videoId", (req, res) => {
-  console.log("URL Param:", req.params);
-
   const videos = readVideos();
   const selectedVideo = videos.find((video) => video.id === req.params.videoId);
 
+  // const filter = videos.filter((video) => {
+  //   return video.id === req.params.videoId;
+  // });
+  // console.log(filter[0].comments);
   if (!selectedVideo) {
     res.status(404).send({
       message: "No video with that id exists",
@@ -61,9 +63,10 @@ router.post("/videos", (req, res) => {
     likes: "320,984",
     duration: "5:06",
     video: "https://project-2-api.herokuapp.com/stream",
-    timestamp: 1626032763000,
+    timestamp: Date.now(),
     comments: [
       {
+        id: uniqid(),
         name: "Micheal Lyons",
         comment:
           "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
@@ -71,6 +74,7 @@ router.post("/videos", (req, res) => {
         timestamp: 1628522461000,
       },
       {
+        id: uniqid(),
         name: "Gary Wong",
         comment:
           "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
@@ -78,6 +82,7 @@ router.post("/videos", (req, res) => {
         timestamp: 1626359541000,
       },
       {
+        id: uniqid(),
         name: "Theodore Duncan",
         comment:
           "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He’s definitely my favorite ever!",
@@ -98,7 +103,54 @@ router.post("/videos", (req, res) => {
   res.json(newVideo);
 });
 
+router.post("/videos/:id/comments", (req, res) => {
+  const newComment = {
+    name: req.body.name,
+    comment: req.body.comment,
+    likes: 0,
+    timestamp: Date.now(),
+    id: uniqid(),
+  };
 
-// router.delete("/videos/:videoId", (req, res) => {}
+  const videos = readVideos();
+
+  const filteredVideo = videos.filter((video) => {
+    return video.id === req.params.id;
+  });
+
+  const comment = filteredVideo[0].comments;
+  console.log(filteredVideo);
+  comment.push(newComment);
+
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+
+  res.json(comment);
+});
+
+router.delete("/videos/:videoId/comments/:commentId", (req, res) => {
+  const videos = readVideos();
+
+  const filteredVideo = videos.filter((video) => {
+    return video.id === req.params.videoId;
+  });
+
+  const comments = filteredVideo[0].comments;
+
+  for (let i = 0; i < comments.length; i++) {
+    if (comments[i].id === req.params.commentId) {
+      comments.splice(i, 1);
+    }
+  }
+
+  // newComments = comments.filter((comment) => {
+  //   return comment.id !== req.params.commentId;
+  // });
+
+  // console.log(newComments);
+
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+
+  res.json(comments);
+});
 
 module.exports = router;
